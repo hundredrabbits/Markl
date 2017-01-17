f4390f64813773ced8220653ad9f3fffeb25defe
Devine Lu Linvega <aliceffekt@gmail.com>
2017-01-17 10:12:03 -0700

    Init

---
+++ scripts/init.js
@@ -0,0 +1,7 @@
+
+markle = new Markle();
+
+markle.set_arena(new Arena());
+markle.set_players(new Player(),new Player(),new Player(),new Player());
+
+markle.start()
---
+++ scripts/markle.js
@@ -0,0 +1,20 @@
+function Markle()
+{
+  this.arena = null;
+  this.players = [];
+
+  this.set_arena = function(arena)
+  {
+    this.arena = arena;
+  }
+
+  this.set_players = function(players)
+  {
+    this.players = players;
+  }
+
+  this.start = function()
+  {
+    
+  }
+}
