cc2b404bad468070018660c33d8971f74347916d
Devine Lu Linvega <aliceffekt@gmail.com>
2018-06-30 15:56:16 +1200

    Starting buttons for IDE

--- ide/scripts/commander.js
+++ ide/scripts/commander.js
@@ -9,23 +9,67 @@ function Commander()
   this.fightscript = new FightScript();
 
   this.runes = []
-  
+
+  // Construction
+  this.construction = []
+
+  // UI
+  this.buttons = []
+
+  this.buttons.push(new Fragment(this,"TRIGGER","SIGHT"))
+  this.buttons.push(new Fragment(this,"TRIGGER","DEFAULT"))
+  this.buttons.push(new Fragment(this,"EVENT","FIGHTER"))
+  this.buttons.push(new Fragment(this,"EVENT","BLOCKER"))
+  this.buttons.push(new Fragment(this,"CONDITION","DISTANCE IS 3"))
+  this.buttons.push(new Fragment(this,"CONDITION","DISTANCE IS 2"))
+  this.buttons.push(new Fragment(this,"ACTION","MOVE LEFT"))
+
   this.install = function(host)
   {
-    host.appendChild(this.el);
     host.appendChild(this.preview);
+    host.appendChild(this.el);
+
+    for(id in this.buttons){
+      var button = this.buttons[id]
+      this.el.appendChild(button.el)
+    }
   }
 
-  this.add = function(rune)
+  this.add_rune = function(rune)
   {
     this.fightscript.add(rune)
     this.update()
   }
 
+  this.add_fragment = function(fragment)
+  {
+    
+    this.construction[fragment.depth] = fragment;
+    this.update();
+    console.log(this.construction);
+  }
+
   this.update = function()
   {
-    this.el.innerHTML = `${this.runes.length} runes`;
     this.preview.innerHTML = this.fightscript.render();
     console.log(this.fightscript.runes());
+  }
+}
+
+function Fragment(host,type,text)
+{
+  this.host = host;
+  this.type = type;
+  this.text = text;
+  this.depth = ["TRIGGER","EVENT","CONDITION","ACTION"].indexOf(this.type)
+  this.el = document.createElement('button');
+  this.el.className = type.toLowerCase();
+  this.el.innerHTML = `${text}`;
+
+  this.el.onclick = () => { this.construct(); }
+
+  this.construct = function()
+  {
+    this.host.add_fragment(this)
   }
 }
