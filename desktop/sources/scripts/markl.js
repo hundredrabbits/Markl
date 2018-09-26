8401310649b7b9d586c9cbdd12d828b4ec8e332c
Devine Lu Linvega <aliceffekt@gmail.com>
2018-09-27 08:49:18 +1200

    Started to implement dragover states

--- desktop/sources/scripts/markl.js
+++ desktop/sources/scripts/markl.js
@@ -2,14 +2,16 @@
 
 function Markl()
 {
-  this.el = document.createElement('yu');
-  this.el.className = "screen";
+  this.el = document.createElement('div');
+  this.el.id = "app";
 
   this.navigator  = new Navigator();
   this.interface  = new Interface();
   this.renderer   = new Renderer();
   this.controller = new Controller();
   this.keyboard = new Keyboard();
+  this.project = new Project();
+
   this.supervisor = require('../server/core/supervisor')
   this.scenario = require('../server/core/scenario')
 
@@ -20,35 +22,6 @@ function Markl()
     this.renderer.install(this.el);
     this.interface.install(this.el);
     this.navigator.install(this.el);
-
-    this.controller.add("default","*","About",() => { require('electron').shell.openExternal('https://github.com/hundredrabbits/Dotgrid'); },"CmdOrCtrl+,");
-    this.controller.add("default","*","Fullscreen",() => { app.toggle_fullscreen(); },"CmdOrCtrl+Enter");
-    this.controller.add("default","*","Hide",() => { app.toggle_visible(); },"CmdOrCtrl+H");
-    this.controller.add("default","*","Inspect",() => { app.inspect(); },"CmdOrCtrl+.");
-    this.controller.add("default","*","Documentation",() => { markl.controller.docs(); },"CmdOrCtrl+Esc");
-    this.controller.add("default","*","Reset",() => { markl.reset(); },"CmdOrCtrl+Backspace");
-    this.controller.add("default","*","Quit",() => { app.exit(); },"CmdOrCtrl+Q");
-
-    this.controller.add_role("default","Edit","undo");
-    this.controller.add_role("default","Edit","redo");
-    this.controller.add_role("default","Edit","cut");
-    this.controller.add_role("default","Edit","copy");
-    this.controller.add_role("default","Edit","paste");
-    this.controller.add_role("default","Edit","delete");
-    this.controller.add_role("default","Edit","selectall");
-    
-    this.controller.add("default","Navigator","Play",() => { markl.navigator.play(); },"CmdOrCtrl+R");
-    this.controller.add("default","Navigator","Pause/Resume",() => { markl.navigator.pause(); },"CmdOrCtrl+P");
-    this.controller.add("default","Navigator","Stop",() => { markl.navigator.stop(); },"CmdOrCtrl+W");
-    this.controller.add("default","Navigator","Next Turn",() => { markl.navigator.next(); },"CmdOrCtrl+Right");
-    this.controller.add("default","Navigator","Prev Turn",() => { markl.navigator.prev(); },"CmdOrCtrl+Left");
-    this.controller.add("default","Navigator","Last Turn",() => { markl.navigator.last(); },"CmdOrCtrl+Shift+Right");
-    this.controller.add("default","Navigator","First Turn",() => { markl.navigator.first(); },"CmdOrCtrl+Shift+Left");
-    this.controller.add("default","Navigator","Toggle View",() => { markl.navigator.toggle(); },"CmdOrCtrl+D");
-    this.controller.add("default","Navigator","Save",() => { markl.navigator.save(); },"CmdOrCtrl+S");
-    this.controller.add("default","Navigator","Export",() => { markl.navigator.export(); },"CmdOrCtrl+E");
-
-    this.controller.commit();
 
     this.keyboard.install();
   }
@@ -68,4 +41,48 @@ function Markl()
   {
     this.el.className = `${this.navigator.is_visible ? "designer_on" : "designer_off"}`;
   }
+
+  function add_class(el,c)
+  {
+    if(has_class(el,c)){ return; }
+    el.className = `${el.className} ${c}`;
+  }
+
+  function remove_class(el,c)
+  {
+    if(!has_class(el,c)){ return; }
+    el.className = `${el.className}`.replace(c,'').trim();
+  }
+
+  function has_class(el,c)
+  {
+    if(el.className.indexOf(c) < 0){ return false; }
+    return true;
+  }
+
+  window.addEventListener('dragover',function(e)
+  {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+
+    add_class(markl.el,"dragover");
+  });
+
+  window.addEventListener('drop', function(e)
+  {
+    e.stopPropagation();
+    e.preventDefault();
+
+    let files = e.dataTransfer.files;
+
+    for(let id in files){
+      let file = files[id];
+      if(!file.path){ continue;}
+      if(file.type && !file.type.match(/text.*/)) { console.log(`Skipped ${file.type} : ${file.path}`); continue; }
+      console.log(file);
+    }
+
+    remove_class(markl.el,"dragover");
+  });
 }
---
+++ desktop/sources/scripts/project.js
@@ -0,0 +1,14 @@
+function Project()
+{
+  this.new = function()
+  {
+    console.log("New FightStyle")
+  }
+
+  this.open = function()
+  {
+    console.log("Open FightStyle")
+  }
+}
+
+module.exports = Project;
