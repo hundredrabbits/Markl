624cd01af2616c23ae07bc766ae91142f152e858
Devine Lu Linvega <aliceffekt@gmail.com>
2018-10-12 13:40:03 +1200

    Added logo

--- desktop/sources/scripts/screens/splash_screen.js
+++ desktop/sources/scripts/screens/splash_screen.js
@@ -3,13 +3,21 @@
 function SplashScreen () {
   Screen.call(this, 'splash')
 
+  this.logo = this._create_el('logo')
+
+  this.install = function (host) {
+    this.el.appendChild(this.logo)
+
+    host.appendChild(this.el)
+  }
+
   this.run = function (speed = 250) {
     if (!markl.scenario || !markl.scenario.script) { console.log('Missing script'); return }
 
     console.log('Found script!')
 
     setTimeout(() => {
-      markl.flow.goto('fighter')
+      // markl.flow.goto('fighter')
     }, speed)
   }
 }
