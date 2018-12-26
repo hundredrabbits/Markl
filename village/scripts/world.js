3cefbdbab11eb40adab95c9a542ee3fc1e054b5d
Devine Lu Linvega <aliceffekt@gmail.com>
2018-12-26 13:40:16 +1200

    Better stage

--- village/scripts/world.js
+++ village/scripts/world.js
@@ -8,6 +8,7 @@ function World (stage) {
       // Tiles
       new SafeTile({ x: 8, y: -4, z: -1 }),
       new SafeTile({ x: 8, y: -8, z: -1 }),
+      new SafeTile({ x: 4, y: -6, z: -1 }),
       new MirrorXTile({ x: 10, y: -6, z: -1 }),
       new MirrorXTile({ x: 6, y: -6, z: -1 }),
       new OverrideTile({ x: 6, y: -2, z: -1 }, [INPUT.up]),
@@ -15,6 +16,7 @@ function World (stage) {
       new WarpTile({ x: 10, y: -8, z: -1 }, 'sewers', { x: 0, y: 0, z: 0 }),
       // Events
       new Chat({ x: 8, y: -3, z: 0 }, 'Hello Traveler'),
+      new Chat({ x: 4, y: -5, z: 0 }, 'Hello Traveler'),
       new Blocker({ x: 8, y: -6, z: 0 })
     ])
   }
@@ -29,7 +31,10 @@ function World (stage) {
   function _sewers () {
     return new Level('sewers', { w: 8, h: 5 }, [
       new WarpTile({ x: 0, y: 0, z: -1 }, 'lobby', { x: 10, y: -8, z: 0 }),
-      new SafeTile({ x: 1, y: 0, z: -1 })
+      new SafeTile({ x: 1, y: 0, z: -1 }),
+      new Blocker({ x: 2, y: 0, z: 0 }),
+      new SafeTile({ x: 5, y: 0, z: -1 }),
+      new Blocker({ x: 4, y: 0, z: 0 }),
     ])
   }
 
